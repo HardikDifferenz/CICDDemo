aaaeb3c9d858edd04d1c5301841399a7613ff18a
HardikDifferenz <hardik.raiyani@differenzsystem.com>
2022-11-10 14:56:26 +0530

    added Github app distri.....

--- app/src/main/java/com/cicddemo/app/MainActivity.kt
+++ app/src/main/java/com/cicddemo/app/MainActivity.kt
@@ -1,16 +1,57 @@
 package com.cicddemo.app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.*
+import com.google.android.gms.tasks.Task
+
 
 class MainActivity : AppCompatActivity() {
+    private var mGoogleSignInClient: GoogleSignInClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val gso: GoogleSignInOptions =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        findViewById<TextView>(R.id.btnLogin).setOnClickListener {
+            signIn()
+        }
+
         //https://medium.com/firebase-developers/quickly-distribute-app-with-firebase-app-distribution-using-github-actions-fastlane-c7d8eca18ee0
     }
+
+    private fun signIn() {
+        //Google Login Reference link
+        //https://developers.google.com/identity/sign-in/android/start-integrating
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        resultLauncher.launch(signInIntent)
+    }
+
+    private var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // There are no request codes
+                val task: Task<GoogleSignInAccount> =
+                    GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                Log.d("Email ", task.result.email ?: "")
+            }else if (result.resultCode == Activity.RESULT_CANCELED){
+                Log.d("Error ", "result")
+            }
+        }
+    // You can do the assignment inside onAttach or onCreate, i.e, before the activity is displayed
 }
+
+
 //Shreyas changes
 /*
 * Fastlanecommand
